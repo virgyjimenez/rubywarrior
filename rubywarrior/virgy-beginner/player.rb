56f898a13d632000dc2fedb0f10eb990a21e3aef
virgyjimenez <miguelvjb_311@hotmail.com>
2016-04-29 16:54:18 -0500

    level 8

--- rubywarrior/virgy-beginner/player.rb
+++ rubywarrior/virgy-beginner/player.rb
@@ -1,5 +1,36 @@
 class Player
   def play_turn(warrior)
-    warrior.walk!
+
+    if warrior.feel.wall?
+      warrior.pivot!
+    elsif warrior.feel(:forward).empty?
+      if warrior_health(warrior) && warrior.health < 12
+        warrior.walk!(:backward)
+      elsif warrior.health < 20 && warrior.feel(:backward).wall?
+        warrior.rest!
+      else
+        warrior.walk!(:forward)
+      end
+    else
+      warrior.attack!(:forward)
+    end
+    @health = warrior.health
+  end
+
+  def warrior_health(warrior)
+    @health = warrior.health if @health.nil?
+    warrior.health < @health
+  end
+
+  def rest?(warrior)
+    warrior_health(warrior) && warrior.feel.empty?
+  end
+
+  def captive?(warrior)
+    if warrior.feel.captive?
+      warrior.rescue!
+    else
+      warrior.attack!
+    end
   end
 end
